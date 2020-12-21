651e96c62cc3124ee1d68f0f196b3289d878b0d3
Evgeny <salabaikin@mail.ru>
2020-12-21 14:29:59 +0300

    Add lesson 18

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    // Timer 
+    function countTimer(deadline) {
+
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds'),
+            dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow)/1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor(timeRemaining / 60);
+            
+    }
+
+    countTimer('30 January 2021');
+
+});
