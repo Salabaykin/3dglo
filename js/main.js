0deb0f46c81483f70b1b31697358dd99baba5879
Evgeny <salabaikin@mail.ru>
2020-12-22 10:14:40 +0300

    Fix timer

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@ window.addEventListener('DOMContentLoaded', function() {
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+
+        const getTimeRemaining = () => {
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow)/1000,
@@ -20,21 +24,21 @@ window.addEventListener('DOMContentLoaded', function() {
             return {hours, minutes, seconds, timeRemaining};
         }
 
-        function updateClock() {
+        const updateClock = () => {
             let timer = getTimeRemaining();
             timerHours.textContent = timer.hours <= 9 && timer.hours  >= 0 ? `0${timer.hours}` : timer.hours;
             timerMinutes.textContent = timer.minutes <= 9 && timer.minutes  >= 0 ? `0${timer.minutes}` : timer.minutes;
             timerSeconds.textContent = timer.seconds <= 9 && timer.seconds  >= 0 ? `0${timer.seconds}` : timer.seconds;
 
             if(timer.hours < 0) {
-                clearInterval(aa);
+                clearInterval(interval);
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
             }
         }
         
-        const aa = setInterval(updateClock, 1000);
+        const interval = setInterval(updateClock, 1000);
     }
 
     countTimer('23 December 2020');
