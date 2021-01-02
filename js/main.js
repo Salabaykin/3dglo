444bc0a4f31e73d96cd329474dc521ae4ec58c1a
Evgeny <salabaikin@mail.ru>
2021-01-02 19:25:55 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ window.addEventListener('DOMContentLoaded', function() {
         updateClock();
     }
 
-    countTimer('30 December 2020');
+    countTimer('5 January 2021');
 
     // Menu 
     const toggleMenu = () => {
@@ -345,11 +345,18 @@ window.addEventListener('DOMContentLoaded', function() {
         form.addEventListener('input', (event) => {
             const target = event.target;
             if (target.matches('.form-phone')) {
-                target.value = target.value.replace(/[^\+\d]/g, '');
+                maskPhone('.form-phone');
             } else if (target.matches('.form-name')) {
-                target.value = target.value.replace(/[^а-я ]/g, '');
+                target.value = target.value.replace(/[^а-я ]/gi, '');
             } else if (target.matches('.mess')) {
                 target.value = target.value.replace(/[^0-9а-я!,-?:;""() ]/gi, '');
+            } else if (target.matches('.form-email')) {
+                let patt = /^\w{2,}@\w{2,}\.\w{2,}$/i;
+                if (!patt.test(target.value)) {
+                    target.style = 'border:3px solid red;';
+                } else {
+                    target.style = 'border:none;';
+                }
             }
         });
 
@@ -361,6 +368,7 @@ window.addEventListener('DOMContentLoaded', function() {
             form.append(statusMessage);
             statusMessage.innerHTML = loadMessage;
             statusMessage.style.color = '#fff';
+            console.log(form);
             const formData = new FormData(form);
             let body = {};
             for (let [key, value] of formData.entries()) {
@@ -368,6 +376,10 @@ window.addEventListener('DOMContentLoaded', function() {
             }
             postData(body, () => {
                 statusMessage.textContent = successMessage;
+                setTimeout(() => {
+                    document.querySelector('.popup').style = 'display:none;';
+                    form.removeChild(statusMessage);
+                }, 3000);
             }, (error) => {
                 statusMessage.textContent = errorMessage;
                 console.error(error);
@@ -395,5 +407,43 @@ window.addEventListener('DOMContentLoaded', function() {
     sendForm('form1');
     sendForm('form2');
     sendForm('form3');
+
+    function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+        const elems = document.querySelectorAll(selector);
+    
+        function mask(event) {
+            const keyCode = event.keyCode;
+            const template = masked,
+                def = template.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, "");
+            let i = 0,
+                newValue = template.replace(/[_\d]/g, function (a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+                });
+            i = newValue.indexOf("_");
+            if (i != -1) {
+                newValue = newValue.slice(0, i);
+            }
+            let reg = template.substr(0, this.value.length).replace(/_+/g,
+                function (a) {
+                    return "\\d{1," + a.length + "}";
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+                this.value = newValue;
+            }
+            if (event.type == "blur" && this.value.length < 5) {
+                this.value = "";
+            }
+    
+        }
+    
+        for (const elem of elems) {
+            elem.addEventListener("input", mask);
+            elem.addEventListener("focus", mask);
+            elem.addEventListener("blur", mask);
+        }
+        
+    }
           
 });
