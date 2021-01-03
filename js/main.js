d32ca4f311aee63a80e86fc065a6c2dd9df717a0
Evgeny <salabaikin@mail.ru>
2021-01-03 21:24:23 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
     // Timer 
-    function countTimer(deadline) {
+    const countTimer = (deadline) => {
 
         let timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
@@ -345,11 +345,11 @@ window.addEventListener('DOMContentLoaded', function() {
         form.addEventListener('input', (event) => {
             const target = event.target;
             if (target.matches('.form-phone')) {
-                maskPhone('.form-phone');
-            } else if (target.matches('.form-name')) {
-                target.value = target.value.replace(/[^а-я ]/gi, '');
+                target.value = target.value.replace(/[^\+\d]/g, '');
+            } else if (target.matches('.form-name') || target.matches('#form2-name')) {
+                target.value = target.value.replace(/^[^А-Яа-яЁё]\s?[^А-Яа-яЁё]?/gi, '');
             } else if (target.matches('.mess')) {
-                target.value = target.value.replace(/[^0-9а-я!,-?:;""() ]/gi, '');
+                target.value = target.value.replace(/^[^0-9А-Яа-яЁё\s!,-?:;""()]/gi, '');
             } else if (target.matches('.form-email')) {
                 let patt = /^\w{2,}@\w{2,}\.\w{2,}$/i;
                 if (!patt.test(target.value)) {
@@ -406,43 +406,5 @@ window.addEventListener('DOMContentLoaded', function() {
     sendForm('form1');
     sendForm('form2');
     sendForm('form3');
-
-    function maskPhone(selector, masked = '+7 (___) ___-__-__') {
-        const elems = document.querySelectorAll(selector);
-    
-        function mask(event) {
-            const keyCode = event.keyCode;
-            const template = masked,
-                def = template.replace(/\D/g, ""),
-                val = this.value.replace(/\D/g, "");
-            let i = 0,
-                newValue = template.replace(/[_\d]/g, function (a) {
-                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-                });
-            i = newValue.indexOf("_");
-            if (i != -1) {
-                newValue = newValue.slice(0, i);
-            }
-            let reg = template.substr(0, this.value.length).replace(/_+/g,
-                function (a) {
-                    return "\\d{1," + a.length + "}";
-                }).replace(/[+()]/g, "\\$&");
-            reg = new RegExp("^" + reg + "$");
-            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
-                this.value = newValue;
-            }
-            if (event.type == "blur" && this.value.length < 5) {
-                this.value = "";
-            }
-    
-        }
-    
-        for (const elem of elems) {
-            elem.addEventListener("input", mask);
-            elem.addEventListener("focus", mask);
-            elem.addEventListener("blur", mask);
-        }
-        
-    }
           
 });
